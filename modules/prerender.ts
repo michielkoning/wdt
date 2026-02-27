abb8eff7f86066e3b76d65e20434e068939c07b5
Michiel Koning <mail@michielkoning.nl>
2026-02-27 19:24:22 +0100

    pre

--- modules/prerender.ts
+++ modules/prerender.ts
@@ -56,7 +56,7 @@ export default defineNuxtModule({
         }
       }
       await fetchPagesByType('shows')
-      await fetchPagesByType('posts')
+      // await fetchPagesByType('posts')
       await fetchPagesByType('pages')
     },
 
